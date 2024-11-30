a3cfa9fe087efcb19840f97aaf64daf245d03eca
Neyaz <neyaz.khan@shiprocket.com>
2024-11-30 23:44:37 +0530

    Food Order-app using React, RTK, Jest, Parcel as Bundler,

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
